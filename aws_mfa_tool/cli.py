71234a4f975c830dbd038e5dbc7c21cdf56a5ad5
Erno Kaikkonen <erno@ampli.fi>
2018-05-04 12:47:33 +0300

    Session duration default to 86400 seconds

--- aws_mfa_tool/cli.py
+++ aws_mfa_tool/cli.py
@@ -131,7 +131,7 @@ def create(
          'you will be prompted for a code')
 @click.option(
     '-d', '--duration',
-    default=3600,
+    default=86400,
     help='STS token TTL in seconds')
 @click.option(
     '-o', '--save-output-profile',
